ffca34e0bcbba639516e7efad8b75781f8e039b2
markcwhitfield <markcwhitfield@gmail.com>
2014-07-12 14:02:32 -0500

    Hiding constructors in Newtypes     * Added constructor functions     * A bit preemptive, just = value constructors for now

--- src/Nunavut/Newtypes.hs
+++ src/Nunavut/Newtypes.hs
@@ -1,6 +1,22 @@
-module Nunavut.Newtypes where
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Nunavut.Newtypes (
+  Activation,
+  ErrorSignal,
+  Weights,
+  Error,
+  mkActiv,
+  mkErrSig,
+  mkWeights,
+  mkError,
+  unActiv,
+  unErrSig,
+  unWeights,
+  unError,
+  DimMatchable
+  )where
 
-import Numeric.LinearAlgebra (Matrix, Vector)
+import Data.Text.Lazy (Text)
+import Numeric.LinearAlgebra (Matrix, Vector, dim, cols)
 
 {--------------------------------------------------------------------------
 -                                 Types                                  -
@@ -9,7 +25,33 @@ newtype Activation = Activ { unActiv :: Vector Double }
 newtype ErrorSignal = ErrSig { unErrSig :: Vector Double }
 newtype Weights = Weights { unWeights :: Matrix Double }
 
+newtype Error = Error { unError :: Text }
+
+
+{--------------------------------------------------------------------------
+-                              Contructors                               -
+--------------------------------------------------------------------------}
+mkActiv :: Vector Double -> Activation
+mkActiv = Activ
+mkErrSig :: Vector Double -> ErrorSignal
+mkErrSig = ErrSig
+mkWeights :: Matrix Double -> Weights
+mkWeights = Weights
+
+mkError :: Text -> Error
+mkError = Error
+
+
+{--------------------------------------------------------------------------
+-                                Classes                                 -
+--------------------------------------------------------------------------}
+class DimMatchable a b where
+  dimsMatch :: a -> b -> Bool
 
 {--------------------------------------------------------------------------
 -                               Instances                                -
 --------------------------------------------------------------------------}
+instance DimMatchable Weights Activation where
+  dimsMatch (Weights m) (Activ v)
+    | dim v == cols m = True
+    | otherwise       = False
