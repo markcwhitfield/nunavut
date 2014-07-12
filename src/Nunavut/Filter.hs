9465e017acd332af3cab17a275b19987072ba76a
markcwhitfield <markcwhitfield@gmail.com>
2014-07-12 17:18:26 -0500

    Filter corrections, norm functions, Jacobian newtype     * Filter.hs         * Corrected runDeriv to Activation->Jacobian         * Hid Filter constructor to enforce derivative law         * Added Softmax filter for export         * TODO: Custom filter constructor with numerical checking                 of derivative law     * Newtypes.hs         * Added Jacobian newtype around Matrix Double         * Added elementwise, norm functions

--- src/Nunavut/Filter.hs
+++ src/Nunavut/Filter.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Nunavut.Filter (
-  Filter(..),
   runFilter,
-  runFDeriv
+  runFDeriv,
+  softmaxFilter,
+  Filter
   ) where
 
 import Control.Lens (makeLenses)
+import Numeric.LinearAlgebra (diag, outer)
 
 import Nunavut.Newtypes
 
@@ -14,6 +16,21 @@ import Nunavut.Newtypes
 --------------------------------------------------------------------------}
 data Filter = Filter {
               _runFilter :: Activation -> Activation,
-              _runFDeriv :: Activation -> Activation
+              _runFDeriv :: Activation -> Jacobian
               }
 makeLenses ''Filter
+
+{--------------------------------------------------------------------------
+-                            Helper Functions                            -
+--------------------------------------------------------------------------}
+softmaxFilter :: Filter
+softmaxFilter = Filter softmax softmaxDeriv
+
+softmax :: Activation -> Activation
+softmax v = elementwise (/ l1Norm v) exponentiated
+  where exponentiated = elementwise (exp . (\e -> e - lInf)) v
+        lInf = infNorm v
+
+softmaxDeriv :: Activation -> Jacobian
+softmaxDeriv v = mkJacob $ diag s - (s `outer` s)
+  where s = unActiv . softmax $ v
--- src/Nunavut/Newtypes.hs
+++ src/Nunavut/Newtypes.hs
@@ -1,23 +1,34 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Nunavut.Newtypes (
   Activation,
   ErrorSignal,
   Weights,
+  Jacobian,
   Error,
   mkActiv,
   mkErrSig,
   mkWeights,
   mkError,
+  mkJacob,
   unActiv,
   unErrSig,
   unWeights,
   unError,
+  unJacob,
+  l1Norm,
+  l2Norm,
+  infNorm,
+  frobNorm,
+  elementwise,
   DimMatchable (..)
   )where
 
 import Control.Lens (to)
 import Data.Text.Lazy (Text)
-import Numeric.LinearAlgebra (Matrix, Vector, dim, cols, rows)
+import Numeric.LinearAlgebra (
+  Matrix, Vector, dim, cols, rows, pnorm, NormType(..),
+  mapVector)
 
 import Nunavut.Util.Dimensions
 
@@ -27,8 +38,20 @@ import Nunavut.Util.Dimensions
 newtype Activation = Activ { unActiv :: Vector Double }
 newtype ErrorSignal = ErrSig { unErrSig :: Vector Double }
 newtype Weights = Weights { unWeights :: Matrix Double }
+newtype Jacobian = Jacob { unJacob :: Matrix Double }
 
 newtype Error = Error { unError :: Text }
+
+data Norm = L1 | L2 | InfNorm | Frob
+
+{--------------------------------------------------------------------------
+-                                Classes                                 -
+--------------------------------------------------------------------------}
+class HasNorm a where
+  pNorm :: Norm -> a -> Double
+class HasVec a where
+  toVec :: a -> Vector Double
+  fromVec :: Vector Double -> a
 
 
 {--------------------------------------------------------------------------
@@ -40,6 +63,8 @@ mkErrSig :: Vector Double -> ErrorSignal
 mkErrSig = ErrSig
 mkWeights :: Matrix Double -> Weights
 mkWeights = Weights
+mkJacob :: Matrix Double -> Jacobian
+mkJacob = Jacob
 
 mkError :: Text -> Error
 mkError = Error
@@ -53,7 +78,45 @@ instance HasOutput Weights where
 instance HasInput Weights where
   inSize = to $ rows . unWeights
 
+instance HasVec Activation where
+  toVec = unActiv
+  fromVec = mkActiv
+instance HasVec ErrorSignal where
+  toVec = unErrSig
+  fromVec = mkErrSig
+
+instance HasNorm Activation where
+  pNorm norm a = pnorm (toNormType norm) (unActiv a)
+instance HasNorm ErrorSignal where
+  pNorm norm e = pnorm (toNormType norm) (unErrSig e)
+
+
 instance DimMatchable Weights Activation where
   dimsMatch (Weights m) (Activ v)
     | dim v == cols m = True
     | otherwise       = False
+
+{--------------------------------------------------------------------------
+-                            Helper Functions                            -
+--------------------------------------------------------------------------}
+l1Norm :: (HasNorm a) => a -> Double
+l1Norm = pNorm L1
+
+l2Norm :: (HasNorm a) => a -> Double
+l2Norm = pNorm L2
+
+infNorm :: (HasNorm a) => a -> Double
+infNorm = pNorm InfNorm
+
+frobNorm :: (HasNorm a) => a -> Double
+frobNorm = pNorm Frob
+
+elementwise :: (HasVec a) => (Double -> Double) -> a -> a
+elementwise f = fromVec . mapVector f . toVec
+
+
+toNormType :: Norm -> NormType
+toNormType InfNorm = Infinity
+toNormType L1 = PNorm1
+toNormType L2 = PNorm2
+toNormType Frob = Frobenius
