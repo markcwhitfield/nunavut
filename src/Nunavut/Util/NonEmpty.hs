f9c3e525ed7f50ad2f8e20cb47d028faa1be2a74
markcwhitfield <markcwhitfield@gmail.com>
2014-07-12 15:58:06 -0500

    Added NonEmpty list utils for feed-forward impl     * FFNet will have NonEmpty list of layers,       will simplify lensing, etc.     * Added _head and _last lenses for NonEmpty

---
+++ src/Nunavut/Util/NonEmpty.hs
@@ -0,0 +1,13 @@
+module Nunavut.Util.NonEmpty where
+
+import Control.Lens (Lens', lens)
+import Data.List.NonEmpty (NonEmpty(..), toList, fromList, (<|))
+
+_head :: Lens' (NonEmpty a) a
+_head = lens (head . toList) setHead
+  where setHead (_ :| ls) v = v :| ls
+
+_last :: Lens' (NonEmpty a) a
+_last = lens (last . toList) setLast
+  where setLast (_ :| []) v = v :| []
+        setLast (l :| ls) v = l <| setLast (fromList ls) v
